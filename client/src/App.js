7ce54925c0f2bf53b6f92ce52f449ef3726f6c54
headice <114868841+headice@users.noreply.github.com>
2025-12-17 22:45:13 +1000

    Use BrowserRouter to allow header anchor navigation

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './Home';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Politika from './Politika';
 import  Footer  from "./components/Footer.jsx"
 
