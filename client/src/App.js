c6e4fa60dff64052786caadee5c6039b55d6694f
headice <114868841+headice@users.noreply.github.com>
2025-12-20 02:52:56 +1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Home } from './Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Politika from './Politika';
-import  Footer  from "./components/Footer.jsx"
+import Footer from "./components/Footer.jsx";
 import { PaymentSuccess } from "./PaymentSuccess";
+
+// Компонент, который живёт внутри Router и отправляет хиты при смене маршрута
+function RouteChangeTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.ym && typeof window.ym === 'function') {
+      window.ym(105921891, 'hit', location.pathname);
+    }
+  }, [location.pathname]);
+
+  return null; // ничего не рендерим
+}
 
 function App() {
   return (
     <Router>
       <div className="App">
+        {/* Трекинг переходов */}
+        <RouteChangeTracker />
+
         <Routes>
           {/* Главная страница */}
           <Route path="/" element={<Home />} />
