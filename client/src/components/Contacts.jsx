64a3714c594a9b058e7691d5ff51c0bb4daac030
headice <114868841+headice@users.noreply.github.com>
2025-12-22 22:57:56 +1000

    Update Contacts.jsx

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -36,7 +36,7 @@ export const Contacts = () => {
                 <img
                   src={Tg}
                   alt="Telegram"
-                  className="w-9 h-9 sm:w-10 sm:h-10 md:w-12 md:h-12 hover:opacity-80 transition-opacity"
+                  className="w-12 h-12 sm:w-12 sm:h-12 md:w-12 md:h-12 hover:opacity-80 transition-opacity"
                 />
               </a>
             </div>
