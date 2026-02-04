f6bb050de8a7e92dccbf5d6094ca2226f152b55f
FunRuna <runasuper7@gmail.com>
2026-02-04 10:58:51 +1000

    zxcUpdate TicketModal.jsx

--- client/src/components/TicketModal.jsx
+++ client/src/components/TicketModal.jsx
@@ -211,7 +211,7 @@ export const TicketModal = ({ open, onClose }) => {
             <p className="mt-4 text-center text-sm text-blue-100 sm:text-base">
               Оплата участия дает право на посещение 2-х дневного интенсива 
             </p>
-            <p className="text-center text-sm text-blue-100 sm:text-base">27.03-28.03.2026.</p>
+            <p className="text-center text-sm text-blue-100 sm:text-base">28.03-29.03.2026.</p>
             <p className="mt-3 text-center text-xs text-blue-200 sm:text-sm">
           Регистрация и выдача бейджей участникам.
 Место: вход в . 
